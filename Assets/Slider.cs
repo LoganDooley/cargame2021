5e384b25bbf42f34198e32135c0689da2b00bcd9
anhduong275 <haanhduong275@gmail.com>
2021-10-09 15:50:55 -0400

    Slider - Logan & Anh
    Made progress bar

---
+++ Assets/Slider.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Slider : MonoBehaviour
+{
+    public UnityEngine.UI.Slider progress_bar;
+    public GameObject level_bar;
+    private float start_x;
+    private float final_x = -11.43f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        level_bar.transform.position = new Vector3(11f, level_bar.transform.position.y, level_bar.transform.position.z);
+        start_x = level_bar.transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float progress = (level_bar.transform.position.x - start_x) / (final_x - start_x);
+        progress_bar.value = progress;
+    }
+}
