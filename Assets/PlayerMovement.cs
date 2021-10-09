0166dc399e6d8e93ab44593b9391171d9cb4f853
samdasoon <stella_j_lee@brown.edu>
2021-10-09 14:08:02 -0400

    stellas updates
    with script

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = new Vector2(3, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
