591fe4a308217aab0c6a1cca631a2fed08cba0a9
Sophia Liu <sophia_liu@brown.edu>
2021-11-20 15:34:02 -0500

    finished lose scene

---
+++ Assets/LoseScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseScript : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject LoseMenu;
+
+    void Start()
+    {        
+        LoseButton();
+    }
+
+    public void BackToMainMenuButton()
+    {
+        // Play Now Button has been pressed, here you can initialize your game (For example Load a Scene called GameLevel etc.)
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MenuScene");
+    }
+
+    public void LoseButton()
+    {
+        LoseMenu.SetActive(true);
+        MainMenu.SetActive(false);
+    }
+}
