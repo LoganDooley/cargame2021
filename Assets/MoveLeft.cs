dad3eb47728d94784fe9801fe84394531db38197
dexmcc <32705908+dexmcc@users.noreply.github.com>
2021-11-20 16:50:39 -0500

    colllider fix

--- Assets/MoveLeft.cs
+++ Assets/MoveLeft.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
 {
-    public float speed = 8f;
+    private float speed = 8f;
     // Start is called before the first frame update
     void Start()
     {
